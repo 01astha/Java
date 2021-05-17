39085b115857429665c5732a78012f87a886e33d
Astha Singh <aastha9198singh@gmail.com>
2021-05-18 02:34:05 +0530

    Demontrating One-Dimentional array

--- src/JavaBasic1DArray.java
+++ src/JavaBasic1DArray.java
@@ -5,17 +5,20 @@ public class JavaBasic1DArray {
     public static void main(String[] args)
     {
         Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
         int size = scan.nextInt();
         int[] arr = new int[size];
 
+        System.out.println("Enter the elements of the array: ");
         for(int i=0; i<size; i++)
         {
             arr[i] = scan.nextInt();
         }
 
+        System.out.println("Here are your elements: ");
         for(int i=0; i<size; i++)
         {
-            System.out.println(arr[i]);
+            System.out.println("arr["+i+"]"+"->" + arr[i]);
         }
     }
 }
