d51a34836a133d8c0bd0a9e024007c8b15c99983
deepak sharma <deepak.sharma@peritus.ai>
2021-05-21 22:28:36 +0530

    merge-conflict: sample commit from main branch

--- src/WelcomeToJava.java
+++ src/WelcomeToJava.java
@@ -3,5 +3,6 @@ public class WelcomeToJava {
             /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
             System.out.println("Hello, World.");
             System.out.println("Hello, Java.");
+            // this is in conflict from main branch
     }
 }
