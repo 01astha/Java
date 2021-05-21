216eba3543842c81827a17ecb0f2a0f60294a79d
deepak sharma <deepak.sharma@peritus.ai>
2021-05-21 22:26:36 +0530

    adding sample comment

--- src/WelcomeToJava.java
+++ src/WelcomeToJava.java
@@ -3,5 +3,6 @@ public class WelcomeToJava {
             /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
             System.out.println("Hello, World.");
             System.out.println("Hello, Java.");
+//          this line is for testing purpose of merge-conflict [some-other-branch]
     }
 }
