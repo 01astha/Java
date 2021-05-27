26134a64462415cee3115d00bddb1eae420819fe
Astha Singh <aastha9198singh@gmail.com>
2021-05-28 00:48:04 +0530

    HackerankPracticeSet

--- src/JavaStdinAndStdout1.java
+++ src/JavaStdinAndStdout1.java
@@ -4,6 +4,7 @@ public class JavaStdinAndStdout1 {
 
         public static void main(String[] args) {
             Scanner scan = new Scanner(System.in);
+
             int a = scan.nextInt();
             int b = scan.nextInt();
             int c = scan.nextInt();
