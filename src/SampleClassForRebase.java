03d981fb4dbe1b7770d2884e1481c4987a4886b3
Astha Singh <aastha9198singh@gmail.com>
2021-05-28 01:43:35 +0530

    Rebase Conflict

--- src/SampleClassForRebase.java
+++ src/SampleClassForRebase.java
@@ -2,5 +2,8 @@ public class SampleClassForRebase {
     public static void main(String[] args) {
         // this is sample class
         // this is line to demonstrate rebase conflict
+
+        int a = 10;
+        int b = 20;
     }
 }
