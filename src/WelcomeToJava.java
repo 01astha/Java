e1b04c0c8830ed2d269b0eba0551fa2a26a370c9
Astha Singh <aastha9198singh@gmail.com>
2021-05-28 01:40:44 +0530

    Demonstrating Rebase conflict

--- src/WelcomeToJava.java
+++ src/WelcomeToJava.java
@@ -2,6 +2,5 @@ public class WelcomeToJava {
         public static void main(String[] args) {
             /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
             System.out.println("Hello, Lets learn Java!!");
-//testing-merge-conflict
     }
 }
