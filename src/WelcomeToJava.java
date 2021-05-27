a7c0582311d95516c376248c4415c4fecfba5f56
Astha Singh <aastha9198singh@gmail.com>
2021-05-28 01:38:26 +0530

    Demonstrating Rabase conflict

--- src/WelcomeToJava.java
+++ src/WelcomeToJava.java
@@ -1,8 +1,7 @@
 public class WelcomeToJava {
         public static void main(String[] args) {
             /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
-            System.out.println("Hello, World.");
-            System.out.println("Hello, Java.");
+            System.out.println("Hello, Lets learn Java!!");
 //testing-merge-conflict
     }
 }
