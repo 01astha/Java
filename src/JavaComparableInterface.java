65d9fd9969b20ca86236bcfacfecfa5587e65e78
Astha Singh <aastha9198singh@gmail.com>
2021-05-29 00:34:54 +0530

    Demostrating Comparable

---
+++ src/JavaComparableInterface.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.util.Collections;
+
+class Employees implements Comparable<Employees>
+{
+    int emp_id, emp_salary;
+    String emp_name;
+
+    public Employees(int emp_id, String emp_name, int emp_salary)
+    {
+        super();
+        this.emp_id = emp_id;
+        this.emp_name = emp_name;
+        this.emp_salary = emp_salary;
+    }
+
+    public String toString(){
+        return "Employees[Employee ID = " + emp_id + " Employee Name = " + emp_name + " Employee Salary = " + emp_salary +"]";
+    }
+
+    public int compareTo(Employees e)
+    {
+        return emp_salary>e.emp_salary?1:-1;
+    }
+}
+
+public class JavaComparableInterface {
+
+    public static void main(String[] args)
+    {
+        List<Employees> emp = new ArrayList<>();
+
+        emp.add(new Employees(1, "John", 98000));
+        emp.add(new Employees(4, "Ben", 80000));
+        emp.add(new Employees(3, "Robin", 130000));
+        emp.add(new Employees(2, "Andrew", 75000));
+
+        Collections.sort(emp);
+
+        for(Employees e : emp)
+        {
+            System.out.println(e);
+        }
+    }
+}
--- src/JavaDatatypes.java
+++ src/JavaDatatypes.java
@@ -12,7 +12,7 @@ class JavaDatatypes{
             try
             {
                 long x=sc.nextLong();
-                System.out.println(x+" can be fitted in:");
+                System.out.println(x +" can be fitted in:");
                 if(x>=-128 && x<=127) System.out.println("* byte");
                 if(x>=Math.pow(-2,15) && (x<=Math.pow(2,15)-1)) System.out.println("* short");
                 if(x>=Math.pow(-2,31) && (x<=Math.pow(2,31)-1)) System.out.println("* int");
